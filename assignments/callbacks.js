916f9bd2f5e0bce65e057e881bec36eb611ebf50
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2019-07-23 19:13:56 -0400

    MVP and part of Stretch finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -29,22 +29,75 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+
+getLength(items, function(length) {
+  console.log(length)
+});
+
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+
+function last(arr, cb) {
+  return cb(arr[arr.length-1]);
+}
+
+last(items, function(last) {
+  console.log(last)
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
+function sumNums(x, y, cb) {
+  return cb(x+y);
+}
+
+sumNums(2, 3, function(sum) {
+  console.log(sum)
+});
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+
+function multiplyNums(x, y, cb) {
+  return cb(x*y);
+}
+
+multiplyNums(3, 3, function(product) {
+  console.log(product)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+function contains(item, list, cb) {
+  for (i=0; i < list.length; i++){
+    if (list[i] === item){
+      return cb(true);
+    }
+  }
+  for (i=0; i < list.length; i++){
+    if (list[i] != item){
+      return cb(false);
+    }
+  }
+  
+}
+
+contains("Gum", items , function(find) {
+  console.log(find)
+});
 
 /* STRETCH PROBLEM */
 
